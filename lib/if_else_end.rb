b826dcf4a5b1c6415c84f6dd8bb787c34a2eb32e
vivacious-revert-7013 <mesmith454@gmail.com>
2020-10-24 22:41:10 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,10 @@
-# Write your solution here
+current_time = Time.now 
+current_time = current_time.to_i 
+
+if current_time % 2 == 0 
+  puts "Even!"
+else
+  puts "Odd!"
+end
+
+#alternate solution changes line 4 to if current_time.even?
---
+++ selection-lab.rb
@@ -0,0 +1,5 @@
+if current_time.to_i.even = TRUE
+  puts "Even!"
+else
+  puts "Odd!"
+end
