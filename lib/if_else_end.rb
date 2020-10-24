7c83f1ebb43f6940817bf5ecb94f23df71583755
vivacious-revert-7013 <mesmith454@gmail.com>
2020-10-24 22:40:14 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,7 +1,8 @@
-current_time = "#{Time.now}.to_i"
-  puts "#{Time.now}"
-if current_time.even ? = true
-  puts 'Even!'
+current_time = Time.now 
+current_time = current_time.to_i 
+
+if current_time % 2 == 0 
+  puts "Even!"
 else
   puts "Odd!"
 end
